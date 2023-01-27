bf7c72a20ac221fb05dbcfe27c72ed36574625f2
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-01-27 19:03:25 -0300

    Requisito 2 Corrigido

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "User",
     {
+      id: { type: DataTypes.STRING, primaryKey: true },
       display_name: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
