8342887a24389c995d25d7f185e1aadced38bf3a
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-01-28 19:49:31 -0300

    Requisito 11 Completo

---
+++ src/models/PostCategory.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    "PostCategory",
+    {
+      category_id: { type: DataTypes.INTEGER, foreignKey: true },
+      post_id: { type: DataTypes.INTEGER, foreignKey: true },
+    },
+    { timestamps: false, tableName: "posts_categories" }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: "blog_posts",
+      through: PostCategory,
+      foreignKey: "category_id",
+      otherKey: "post_id",
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: "categories",
+      through: PostCategory,
+      foreignKey: "post_id",
+      otherKey: "category_id",
+    });
+  };
+
+  return PostCategory;
+};
